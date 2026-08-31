68cd001783b7ea473ecfb8240dffe8d0945b81b6
GabBois <gab.bois.pro@gmail.com>
2026-08-31 11:25:57 +0200

    HotReload + switch theme logic

--- Assets/Scripts/Theme.cs
+++ Assets/Scripts/Theme.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,19 +7,68 @@ public class Theme : MonoBehaviour
 {
     [SerializeField] private List<GameObject> nodeList;
     [SerializeField] private float zoneSize;
+    [SerializeField] private float maxNodePerLine;
+    [SerializeField] private Vector2 nodeSpacing;
+    [SerializeField] private float disableDuration;
+    [SerializeField] private float enableDuration;
 
-    //TODO: système de placement des nodes dans la zone 
+    private Coroutine enableThemeRoutine;
+    //
     
     public void Initialise()
     {
-        
+        enableThemeRoutine = StartCoroutine(ActiveTheme());
     }
 
+    [ContextMenu("Replace Nodes")]
     void ReplaceNodes()
     {
-        
+        int estimedColumn = Mathf.CeilToInt(nodeList.Count / maxNodePerLine);
+
+        Vector3 startPos = transform.position - new Vector3(zoneSize / 2, 0, -zoneSize / 2);
+        for (int c = 0; c < estimedColumn; c++)
+        {
+            
+            for (int i = 0; i < maxNodePerLine; i++)
+            {
+                int nodeIndex = c * (int)maxNodePerLine + i;
+                if (nodeIndex >= nodeList.Count) continue;
+                
+                Vector3 nodePos = startPos + new Vector3(nodeSpacing.x * i, 0, 0);
+                nodeList[nodeIndex].transform.position = nodePos;
+            }
+            startPos.z -= nodeSpacing.y;
+        }
+
     }
 
+    public IEnumerator DisableTheme()
+    {
+        if(enableThemeRoutine != null)
+        {
+            StopCoroutine(enableThemeRoutine);
+            enableThemeRoutine = null;
+        }
+        float nodeDisablingDuration = disableDuration / nodeList.Count;
+        for (int i = 0; i < nodeList.Count; i++)
+        {
+            nodeList[i].SetActive(false);
+            yield return new WaitForSeconds(nodeDisablingDuration);
+        }
+        yield return null;
+    }
+
+    IEnumerator ActiveTheme()
+    {
+        float nodeEnablingDuration = enableDuration / nodeList.Count;
+        for (int i = 0; i < nodeList.Count; i++)
+        {
+            nodeList[i].SetActive(true);
+            yield return new WaitForSeconds(nodeEnablingDuration);
+        }
+        yield return null;
+    }
+    
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.darkGreen;
--- Assets/Scripts/UI/ThemeManager.cs
+++ Assets/Scripts/UI/ThemeManager.cs
@@ -1,6 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 public class ThemeManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] themePanelList;
+    private Theme activeTheme;
+    private Coroutine disableThemeRoutine;
+
+    private void Start()
+    {
+        activeTheme = themePanelList[0].GetComponent<Theme>();
+    }
+
+    public void OpenTheme(int _index)
+    {
+        if (disableThemeRoutine != null)
+        {
+            StopCoroutine(disableThemeRoutine);
+        }
+
+        disableThemeRoutine = StartCoroutine(DisableThemeSequence(_index));
+
+    }
+
+    IEnumerator DisableThemeSequence(int _index)
+    {
+        if (activeTheme)
+        {
+            yield return activeTheme.DisableTheme();
+        }
+        themePanelList[_index].SetActive(true);
+        themePanelList[_index].TryGetComponent(out activeTheme);
+        activeTheme.Initialise();
+    }
 }
